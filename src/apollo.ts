532e4910ce3d4a752be40f01c661d649471f87f9
WebcrazyDylan <webcrazy@naver.com>
2021-02-19 10:24:34 -0800

    wss https

--- src/apollo.ts
+++ src/apollo.ts
@@ -15,7 +15,10 @@ export const isLoggedInVar = makeVar(Boolean(token));
 export const authTokenVar = makeVar(token);
 
 const wsLink = new WebSocketLink({
-  uri: `ws://localhost:4000/graphql`,
+  uri:
+    process.env.NODE_ENV === "production"
+      ? "wss://nuber-eats-backend-nest-2021.herokuapp.com/graphql"
+      : `ws://localhost:4000/graphql`,
   options: {
     reconnect: true,
     connectionParams: {
@@ -25,7 +28,10 @@ const wsLink = new WebSocketLink({
 });
 
 const httpLink = createHttpLink({
-  uri: "http://localhost:4000/graphql"
+  uri:
+    process.env.NODE_ENV === "production"
+      ? "https://nuber-eats-backend-nest-2021.herokuapp.com/graphql"
+      : "http://localhost:4000/graphql"
 });
 
 const authLink = setContext((_, { headers }) => {
